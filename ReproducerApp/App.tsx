cf2214baafee2c52076bc077324cdacb329a89cc
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-06-06 14:57:49 +0200

    Add reproducer

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,131 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useReducedMotion,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const colors = [
+  ['lime', 'green'],
+  ['blue', 'cyan'],
+];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function useLoop() {
+  const sv = useSharedValue(0);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    sv.value = 0;
+    sv.value = withRepeat(withTiming(1, {duration: 1000}), -1, true);
+  }, [sv]);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  return sv;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const N = 20;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export default function ChessboardExample() {
+  const [state, setState] = React.useState(0);
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const sv = useLoop();
+
+  const shouldReduceMotion = useReducedMotion();
+
+  useEffect(() => {
+    if (shouldReduceMotion) {
+      return;
+    }
+    const id = setInterval(() => {
+      setState(s => 1 - s);
+    }, 0);
+    return () => clearInterval(id);
+  }, [shouldReduceMotion]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: 10 + sv.value * 20,
+      height: 10 + sv.value * 20,
+    };
+  }, []);
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <View style={styles.workaround} collapsable={false}>
+      <View style={styles.chessboard}>
+        <View style={styles.border}>
+          {[...Array(N).keys()].map(i => (
+            <View style={styles.row} key={i}>
+              {[...Array(N).keys()].map(j => (
+                <Animated.View
+                  key={j}
+                  style={[
+                    {backgroundColor: colors[state % 2][(i + j) % 2]},
+                    animatedStyle,
+                  ]}
+                />
+              ))}
+            </View>
+          ))}
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  workaround: {
+    height: 400,
+    // prevents calling _state->updateState from RNScreens after each change because of view flattening
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  chessboard: {
+    alignItems: 'flex-start',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  border: {
+    borderWidth: 10,
+    borderColor: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  row: {
+    flexDirection: 'row',
   },
 });
-
-export default App;
